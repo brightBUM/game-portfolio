26727e0fd3dfcfb6c0794e4fbda4f7e06093ca59
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2026-09-16 00:41:55 +0530

    Fix GitHub Pages router base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
-
     return (
-
-        <BrowserRouter>
-
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
-
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
-
+                <Route path="/" element={<Home />} />
                 <Route
                     path="/project/:id"
                     element={<ProjectDetails />}
                 />
-
             </Routes>
-
         </BrowserRouter>
-
     );
 }
 
